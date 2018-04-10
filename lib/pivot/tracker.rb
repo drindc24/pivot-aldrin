72a15a5328ec5dc3c5442a4e86a5efea2416ab9e
Aldrin dela Cruz <aldrin@kyck.com>
2018-04-10 18:38:21 +0800

    Added implementation for tracker.rb

--- lib/pivot/tracker.rb
+++ lib/pivot/tracker.rb
@@ -1,5 +1,25 @@
 module Pivot
   class Tracker
-    # Code here!
+    def self.count(items)
+      items.count
+    end
+
+    def self.item_for(items, email)
+      items.reverse.detect{|item| item[:assignee] == email}
+    end
+
+    def self.pivoted?(items, email)
+      items.select{|item| item[:assignee] == email}.any?
+    end
+
+    def self.total_points(items, options = {})
+      return items.select{|item| item[:assignee] == options[:assignee]}.map{|item| item[:points]}.inject(:+) if options[:assignee]
+
+      self.unique_assignees(items).map{|item| item[:points]}.inject(:+)
+    end
+
+    def self.unique_assignees(items)
+      items.reverse.uniq! {|item| item[:assignee]}.reverse
+    end
   end
 end
