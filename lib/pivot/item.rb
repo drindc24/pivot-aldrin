67970b52efc00208b77af7f715fdd6b3a3da97e7
Aldrin dela Cruz <aldrin@kyck.com>
2018-04-10 18:39:12 +0800

    Added implementation for item.

--- lib/pivot/item.rb
+++ lib/pivot/item.rb
@@ -1,5 +1,21 @@
 module Pivot
   class Item
-    # Code here!
+    attr_accessor :name, :assignee, :points
+
+    def initialize(params = {})
+      params.each{ |key, value| send("#{key}=", value) }
+    end
+
+    def project_code
+      name.split("-")[0]
+    end
+
+    def +(value)
+      points + value.points
+    end
+
+    def valid?
+      ["EREC", "AZR"].any?{|code| name.include?(code)}
+    end
   end
 end
