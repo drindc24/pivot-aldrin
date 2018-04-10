7e972ee9bc89de177d16ae572946ef81ebfc518d
Aldrin dela Cruz <aldrin@kyck.com>
2018-04-10 18:41:45 +0800

    Added implementation for person.

--- lib/pivot/person.rb
+++ lib/pivot/person.rb
@@ -1,5 +1,18 @@
 module Pivot
   class Person
-    # Code here!
+    attr_accessor :items, :email, :first_name, :last_name
+
+    def initialize(params = {})
+      params.each{ |key, value| send("#{key}=", value) }
+
+      @items = []
+    end
+
+    def add_item(item, &block)
+      item.assignee = email
+      items << item
+
+      block.call if block_given?
+    end
   end
 end
